563c2daa7663c9b162af77f717fd3659b83154dc
BerkieBb <82737367+BerkieBb@users.noreply.github.com>
2022-03-18 23:42:21 +0100

    fix: update github info

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,15 +6,15 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
+  title: 'New Wheel Documentation',
+  tagline: 'Documentation',
+  url: 'https://roleplayrevisited.github.io/dev-docs/',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'RoleplayRevisited', // Usually your GitHub org/user name.
+  projectName: `dev-docs`, // Usually your repo name.
 
   presets: [
     [
